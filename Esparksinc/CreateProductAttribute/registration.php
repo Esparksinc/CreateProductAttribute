88a2694b8ce4b55f30b4d991ca9810d46a165a43
UmairNasirAhmad29 <ahmed.riaz@esparksinc.com>
2023-06-20 10:47:17 +0500

    Creating custom product attribute programmatically

---
+++ Esparksinc/CreateProductAttribute/registration.php
@@ -0,0 +1,8 @@
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Esparksinc_CreateProductAttribute',
+    __DIR__
+);
